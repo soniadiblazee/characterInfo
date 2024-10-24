61c13b8687f55a76f9b69c7bb93d42ffd2cd85a6
soniadiblazee <cindylol.11037@gmail.com>
2024-10-24 09:16:34 -0400

    y

---
+++ charInfo.py
@@ -0,0 +1,8 @@
+# put what is needed regarding character and setting info
+name = input("Name: ")
+age = input("age: ")
+candy = input("favorite halloween candy: ")
+trait = input("character personalityy traits (e.g., kind, disruptive, curious, etc.): ")
+ff = input("fun fact about the character: ")
+location = input("location of which the storuy takes place: ")
+
